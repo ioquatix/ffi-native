d26bce1427a082702410e1f7cee0ac015e29ec97
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-04-03 18:10:59 +1300

    Fix handling of ffi load order.

--- lib/ffi/module.rb
+++ lib/ffi/module.rb
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+require 'ffi'
+
 require_relative 'module/version'
 
 require_relative 'module/library'
--- lib/ffi/module/version.rb
+++ lib/ffi/module/version.rb
@@ -20,8 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require 'ffi'
-
 module FFI
 	module Module
 		VERSION = "0.1.0"
